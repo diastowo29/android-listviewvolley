44f47af78ad78c30d1ff8354aff06058bbcef4b2
diastowo29 <diastowo@gmail.com>
2016-04-06 15:46:34 +0700

    initial commit

---
+++ app/src/main/java/listviewvolley/example/diastowo/listviewvolley/MainActivity.java
@@ -0,0 +1,13 @@
+package listviewvolley.example.diastowo.listviewvolley;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
